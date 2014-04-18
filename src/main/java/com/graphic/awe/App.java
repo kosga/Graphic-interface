3703539355bb7112a980fa4dc0d9f04c5d3ae50e
kosga <akoschristin@gmail.com>
2014-04-18 17:54:31 +0100

    Mon projet

---
+++ src/main/java/com/graphic/awe/App.java
@@ -0,0 +1,14 @@
+package com.graphic.awe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Bonjour de la part de AWe!" );
+    }
+}
---
+++ src/main/java/com/graphic/awe/test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.graphic.awe;
+
+/**
+ *
+ * @author awe
+ */
+public class test {
+    int taille;
+    Long largeur;
+    
+}
